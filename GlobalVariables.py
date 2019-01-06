00a6e9ea0c249c38dd1a5d9737e1b3edcc9e5743
abrahamwoubie <abraham.woubie@gmail.com>
2019-01-06 04:48:39 +0200

    Updating

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -11,8 +11,8 @@ class GlobalVariables :
     use_CNN_2D=0
     #Grid Size
 
-    nRow = 2
-    nCol = 2
+    nRow = 20
+    nCol = 20
 
     start=1  #0 for fixed start position and 1 for random start position
     goal=1 #0 for fixed goal position and 1 for random goal position
@@ -25,7 +25,7 @@ class GlobalVariables :
     raw_data_state_size= 100
     action_size = 4
     batch_size = 32
-    Number_of_episodes=10
-    timesteps=10#50#(nRow+nCol+nRow)
-    how_many_times = 1 #How many times to run the same experiment
+    Number_of_episodes=100
+    timesteps=200#50#(nRow+nCol+nRow)
+    how_many_times = 10 #How many times to run the same experiment
 
